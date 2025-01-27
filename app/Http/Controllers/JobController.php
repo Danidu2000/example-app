47ceacad139b839668b51cfea99d40461aa70337
Danidu2000 <danidupramuditha2000@gmail.com>
2025-01-27 11:09:44 +0530

    24

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,8 @@ use App\Models\Job;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\JobPosted;
 
 class JobController extends Controller
 {
@@ -35,11 +37,16 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+
+            Mail::to($job->employer->user)->send(
+                new JobPosted($job)
+            );
 
         return redirect('/jobs');
     }
